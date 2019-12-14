19b2680150d7bf942555cc7d995d044afc8fce03
h10n <nur10hakim@gmail.com>
2019-12-14 19:10:10 +0800

    edit custom skript di transaksi

--- resources/views/transactions/index.blade.php
+++ resources/views/transactions/index.blade.php
@@ -28,28 +28,31 @@
 @endsection
 @section('scripts')
   {!! $html->scripts() !!}
-  <script type="text/javascript">
-    $(function(){
-      $('\
-      <div id="filter_status" class="dataTables_length" style="display: inline-block; margin-left:10px;">\
-        <label>Status \
-        <select size="1" name="filter_status" aria-controls="filter_status" \
-            class="form-control input-sm" style="width: 140px;">\
-                <option value="all" selected="selected">Semua</option>\
-                <option value="returned">Sudah Dikembalikan</option>\
-                <option value="not-returned">Belum Dikembalikan</option>\
-            </select>\
-        </label>\
-      </div>\
-      ').insertAfter('.dataTables_length');
-
-    $("#dataTableBuilder").on('preXhr.dt', function(e, settings, data){
-      data.status = $('select[name="filter_status"]').val();
-    });
-
-    $('select[name="filter_status"]').change(function(){
-      window.LaravelDataTables["dataTableBuilder"].ajax.reload();
-    });
-  });
-  </script>
 @endsection
+
+@push('req-scripts')
+<script type="text/javascript">
+  $(function(){
+    $('\
+    <div id="filter_status" class="dataTables_length" style="display: inline-block; margin-left:10px;">\
+      <label>Status \
+      <select size="1" name="filter_status" aria-controls="filter_status" \
+          class="form-control input-sm" style="width: 140px;">\
+              <option value="all" selected="selected">Semua</option>\
+              <option value="returned">Sudah Dikembalikan</option>\
+              <option value="not-returned">Belum Dikembalikan</option>\
+          </select>\
+      </label>\
+    </div>\
+    ').insertAfter('.dataTables_length');
+
+  $("#dataTableBuilder").on('preXhr.dt', function(e, settings, data){
+    data.status = $('select[name="filter_status"]').val();
+  });
+
+  $('select[name="filter_status"]').change(function(){
+    window.LaravelDataTables["dataTableBuilder"].ajax.reload();
+  });
+});
+</script>
+@endpush
