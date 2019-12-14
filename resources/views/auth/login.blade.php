59f1926a637f754840b8aa78af90b534c5066020
h10n <nur10hakim@gmail.com>
2019-12-14 19:01:30 +0800

    rapikan form login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,11 +7,10 @@
     <meta name="author" content="">
     <title>Aplikasi Perpustakaan</title>
     <link rel="stylesheet" href="{{asset('bower_components/bootstrap/dist/css/bootstrap.min.css') }}">
-	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round">
-	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <link href="{{ asset('css/login.css') }}" rel="stylesheet">
 </head>
-
 <body style="background:#ECF0F5;">
     <section>
         <div class="signinpanel">
@@ -20,11 +19,16 @@
                     <div class="signin-info">
                         <div class="thumbnail">
                             <a href="http://localhost/sisfoatlet/public/home">
- {!! Html::image(asset('img/logo/'.$logo),null,['class' => 'img-responsive']) !!}
-              <div class="caption text-center">
-                <h5><strong>Selamat Datang di Aplikasi Perpustakaan {{ $nama_perpus }}, Silahkan Gunakan Username dan Password Anda Untuk Masuk ke Aplikasi!</strong></h5>
-              </div>
-            </a>
+                                {!! Html::image(asset('img/logo/'.$logo),null,['class' => 'img-responsive']) !!}
+                                <div class="caption text-center">
+                                    <h5>
+                                        <strong>
+                                            Selamat Datang di Aplikasi Perpustakaan {{ $nama_perpus }}, Silahkan
+                                            Gunakan Username dan Password Anda Untuk Masuk ke Aplikasi!
+                                        </strong>
+                                    </h5>
+                                </div>
+                            </a>
                         </div>
                     </div>
                 </div>
@@ -33,82 +37,52 @@
                 <div class="col-md-6 col-md-offset-3">
                     <div class="wrapper">
                         <div class="container">
-
-                      {!! Form::open(['route' => 'login','class' => 'form-horizontal']) !!}
-                        {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('username') ? ' has-error' : '' }}">
-
-                            <!--    <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus> -->
+                            {!! Form::open(['route' => 'login','class' => 'form-horizontal']) !!}
+                            {{ csrf_field() }}
+                            <div class="form-group{{ $errors->has('username') ? ' has-error' : '' }}">                            
                                 {!! Form::text('username',null,['class' => 'field-input']) !!}
-                                <!--
-                                @if ($errors->has('username'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('username') }}</strong>
-                                    </span>
-                                @endif
+                                {!! $errors->first('username','<span class="help-block"><strong>:message</strong></span>') !!}
+                            </div>
 
-                              -->
-                              {!! $errors->first('username','<span class="help-block"><strong>:message</strong></span>') !!}
-
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-
-                                <!-- <input id="password" type="password" class="form-control" name="password" required> -->
+                            <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
                                 {!! Form::password('password',['class' => 'field-input']) !!}
-
-                                <!--
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                              -->
-                              {!! $errors->first('password', '<span class="help-block"><strong>:message</strong></span>') !!}
-
-                        </div>
-                                <button type="submit" id="login-button" class="btn btn-primary">
-                                    Login
-                                </button>
-                                <div class="row">
+                                {!! $errors->first('password', '<span class="help-block"><strong>:message</strong></span>') !!}
+                            </div>
+                            <button type="submit" id="login-button" class="btn btn-primary">
+                                Login
+                            </button>
+                            <div class="row">
                                 <a class="btn btn-link" href="{{ route('password.request') }}">
                                     Lupa Password?
                                 </a>
-                              </div>
+                            </div>
+                            {!! Form::close() !!}
+                        </div>
 
-
-
-                    {!! Form::close() !!}
-                  </div>
-
-                  <ul class="bg-bubbles">
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                  </ul>
-              </div>
-          </div>
-      </div>
-      <div class="row">
-          <div class="col-md-6 col-md-offset-3">
-              <div class="signup-footer">
-
-                  <p>Copyright © 2018 Nur Hakim. All rights reserved.</p>
-
-                  <p>Created By: <a href="#" target="_blank">Nur Hakim</a></p>
-
-              </div>
-          </div>
-      </div>
-    </div>
+                        <ul class="bg-bubbles">
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6 col-md-offset-3">
+                    <div class="signup-footer">
+                        <p>Copyright © 2018 Nur Hakim. All rights reserved.</p>
+                        <p>Created By: <a href="#" target="_blank">Nur Hakim</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </section>
-    </body>
-    </html>
+</body>
+</html>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,30 +8,30 @@
                 <div class="panel-heading">Register</div>
 
                 <div class="panel-body">
-                      {!! Form::open(['route' => 'register','class' => 'form-horizontal']) !!}
-                        {{ csrf_field() }}
+                    {!! Form::open(['route' => 'register','class' => 'form-horizontal']) !!}
+                    {{ csrf_field() }}
 
-                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            {!! Form::label('name','Nama',['class'=>'col-md-4 control-label']) !!}
-                            <div class="col-md-6">
-                                <!-- <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus> -->
+                    <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+                        {!! Form::label('name','Nama',['class'=>'col-md-4 control-label']) !!}
+                        <div class="col-md-6">
+                            <!-- <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus> -->
 
-                                {!! Form::text('name',null,['class' => 'form-control']) !!}
-                                <!--
+                            {!! Form::text('name',null,['class' => 'form-control']) !!}
+                            <!--
                                 @if ($errors->has('name'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('name') }}</strong>
                                     </span>
                                 @endif
                               -->
-                              {!! $errors->first('name','<span class="help-block"><strong>:message</strong></span>') !!}
-                            </div>
+                            {!! $errors->first('name','<span class="help-block"><strong>:message</strong></span>') !!}
                         </div>
+                    </div>
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            {!! Form::label('email','Alamat Email',['class' => 'col-md-4 control-label']) !!}
-                            <div class="col-md-6">
-                              <!--
+                    <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                        {!! Form::label('email','Alamat Email',['class' => 'col-md-4 control-label']) !!}
+                        <div class="col-md-6">
+                            <!--
                                 <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
 
                                 @if ($errors->has('email'))
@@ -40,15 +40,15 @@
                                     </span>
                                 @endif
                               -->
-                              {!! Form::email('email',null,['class' => 'form-control']) !!}
-                              {!! $errors->first('email','<span class="help-block"><strong>:message</strong></span>') !!}
-                            </div>
+                            {!! Form::email('email',null,['class' => 'form-control']) !!}
+                            {!! $errors->first('email','<span class="help-block"><strong>:message</strong></span>') !!}
                         </div>
+                    </div>
 
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            {!! Form::label('password','Password',['class' => 'col-md-4 control-label']) !!}
-                            <div class="col-md-6">
-                              <!--
+                    <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                        {!! Form::label('password','Password',['class' => 'col-md-4 control-label']) !!}
+                        <div class="col-md-6">
+                            <!--
                                 <input id="password" type="password" class="form-control" name="password" required>
 
                                 @if ($errors->has('password'))
@@ -57,29 +57,32 @@
                                     </span>
                                 @endif
                               -->
-                              {!! Form::password('password',['class' => 'form-control']) !!}
-                              {!! $errors->first('password','<span class="help-block"><strong>:message</strong></span>') !!}
-                            </div>
+                            {!! Form::password('password',['class' => 'form-control']) !!}
+                            {!! $errors->first('password','<span class="help-block"><strong>:message</strong></span>')
+                            !!}
                         </div>
+                    </div>
 
-                        <div class="form-group">
-                            {!! Form::label('password_confirmation','Konfirmasi Password',['class' => 'col-md-4 control-label']) !!}
-                            <div class="col-md-6">
-                              <!--
+                    <div class="form-group">
+                        {!! Form::label('password_confirmation','Konfirmasi Password',['class' => 'col-md-4
+                        control-label']) !!}
+                        <div class="col-md-6">
+                            <!--
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
                               -->
-                              {!! Form::password('password_confirmation',['class' => 'form-control']) !!}
-                              {!! $errors->first('password_confirmation','<span class="help-block"><strong>:message</strong></span>') !!}
-                            </div>
+                            {!! Form::password('password_confirmation',['class' => 'form-control']) !!}
+                            {!! $errors->first('password_confirmation','<span
+                                class="help-block"><strong>:message</strong></span>') !!}
                         </div>
+                    </div>
 
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Daftar
-                                </button>
-                            </div>
+                    <div class="form-group">
+                        <div class="col-md-6 col-md-offset-4">
+                            <button type="submit" class="btn btn-primary">
+                                Daftar
+                            </button>
                         </div>
+                    </div>
                     {!! Form::close() !!}
                 </div>
             </div>
