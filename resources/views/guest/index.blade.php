a575c1eea56d03755683d9f32b0e83e29cae817d
h10n <nurhakim.nh10@gmail.com>
2021-09-14 00:02:32 +0800

    index

--- resources/views/guest/index.blade.php
+++ resources/views/guest/index.blade.php
@@ -1,67 +1,118 @@
-@extends('layouts.app')
-@section('content')
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 
-<section class="slider-container">
-  <div id="myCarousel" class="carousel slide" data-ride="carousel">
-    <!-- Indicators -->
-    <ol class="carousel-indicators">
-      @foreach($carousel as $carousels)
-      <li data-target="#myCarousel" data-slide-to="{{ $loop->index}}" class="{{ $loop->first ? 'active' : '' }}"></li>
-      @endforeach
-    </ol>
+    <title>{{ $nama_perpus }}</title>
+    @include('layouts._styles')    
+</head>
+<!-- ADD THE CLASS layout-top-nav TO REMOVE THE SIDEBAR. -->
+<body class="hold-transition skin-red-light layout-top-nav">
+<div class="wrapper">
 
-    <!-- Wrapper for slides -->
-    <div class="carousel-inner" role="listbox">
-      @foreach($carousel as $carousel)
-      <div class="item {{ $loop->first ? 'active' : '' }}">
-        <img src="img/slider/{{$carousel->img}}" alt="{{$carousel->title}}" class="img-carousel-rule">
-        <div class="carousel-caption">
-          <h3>{{$carousel->title}}</h3>
-          <p>{{$carousel->text}}</p>
+  <header class="main-header">
+    <nav class="navbar navbar-static-top">
+      <div class="container">
+        <div class="navbar-header">
+          <a href="{{ url('/')}}" class="navbar-brand"><b>APL</b>Perpustakaan</a>         
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
+            <i class="fa fa-bars"></i>
+          </button>
         </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse pull-left" id="navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>                      
+          </ul>         
+        </div>
+        <!-- /.navbar-collapse -->
+        <!-- Navbar Right Menu -->
+        <div class="navbar-custom-menu">
+          <ul class="nav navbar-nav">                   
+          
+          </ul>
+        </div>
+        <!-- /.navbar-custom-menu -->
       </div>
-      @endforeach
+      <!-- /.container-fluid -->
+    </nav>
+  </header>
+  <!-- Full Width Column -->
+  <div class="content-wrapper">
+    <div class="container">
+      <!-- Content Header (Page header) -->
+      <section class="content-header">
+        <h1>
+          Top Navigation
+          <small>Example 2.0</small>
+        </h1>
+        <ol class="breadcrumb">
+          <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+          <li><a href="#">Layout</a></li>
+          <li class="active">Top Navigation</li>
+        </ol>
+      </section>
+
+      <!-- Main content -->
+      <section class="content">
+        <div class="callout callout-info">
+          <h4>Tip!</h4>
+
+          <p>Add the layout-top-nav class to the body tag to get this layout. This feature can also be used with a
+            sidebar! So use this class if you want to remove the custom dropdown menus from the navbar and use regular
+            links instead.</p>
+        </div>
+        <div class="callout callout-danger">
+          <h4>Warning!</h4>
+
+          <p>The construction of this layout differs from the normal one. In other words, the HTML markup of the navbar
+            and the content will slightly differ than that of the normal layout.</p>
+        </div>
+        <div class="box box-default">
+          <div class="box-header with-border">
+            <h3 class="box-title">Blank Box</h3>
+          </div>
+          <div class="box-body">
+            The great content goes here
+          </div>
+          <!-- /.box-body -->
+        </div>
+        <!-- /.box -->
+      </section>
+      <!-- /.content -->
     </div>
-
-    <!-- Left and right controls -->
-    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-      <span class="sr-only">Next</span>
-    </a>
+    <!-- /.container -->
   </div>
-
-</section>
-<section class="content container-fluid">
-  <div class="box">
-    <div class="box-header">
-      <h3 class="box-title">Daftar Buku</h3>
-      <div class="table-button-custom">      
-        <a class="btn bg-olive" onClick="window.location.reload();"><span class="ion-refresh"> Refresh</span></a>        
+  <!-- /.content-wrapper -->
+  <footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>Version</b> 2.4.0
       </div>
+      <strong>Copyright &copy; 2014-2016 <a href="https://adminlte.io">Almsaeed Studio</a>.</strong> All rights
+      reserved.
     </div>
+    <!-- /.container -->
+  </footer>
+</div>
+<!-- ./wrapper -->
 
-    <div class="box-body">
-      {!! $html->table(['class' => 'table table-striped table-hover']) !!}
-    </div>
-  </div>
-</section>
-@endsection
-@section('scripts')
-{!! $html->scripts() !!}
-@endsection
-@push('req-scripts')
-<script>
-  //script form visitor
-  $('#jenis_anggota').change(function () {
-      if ($(this).val() === 'siswa/i') {
-          $('.kelas').show();
-      } else {
-          $('.kelas').hide();
-      }
-  });
-</script>
-@endpush
+<!-- jQuery 3 -->
+<script src="../../bower_components/jquery/dist/jquery.min.js"></script>
+<!-- Bootstrap 3.3.7 -->
+<script src="../../bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<!-- SlimScroll -->
+<script src="../../bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+<!-- FastClick -->
+<script src="../../bower_components/fastclick/lib/fastclick.js"></script>
+<!-- AdminLTE App -->
+<script src="../../dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="../../dist/js/demo.js"></script>
+</body>
+</html>
