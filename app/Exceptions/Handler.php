b071ffb8248d9ca92796596ceb526cfd8003783b
h10n <nurhakim.nh10@gmail.com>
2021-09-22 01:02:23 +0800

    form submit session timeout direct login

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -5,6 +5,8 @@ namespace App\Exceptions;
 use Exception;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Session\TokenMismatchException; 
+
 
 class Handler extends ExceptionHandler
 {
@@ -44,6 +46,11 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if ($exception instanceof TokenMismatchException){
+            // Catch CSRF token mismatch error on session timeout / form
+            return redirect()->route('login');
+        }
+
         return parent::render($request, $exception);
     }
 
