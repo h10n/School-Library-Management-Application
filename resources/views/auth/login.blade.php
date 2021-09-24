f194a9cd1db89f92931c2e209a4f35f30501d8c5
h10n <nurhakim.nh10@gmail.com>
2021-09-24 21:54:27 +0800

    +ganti login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,82 +1,63 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ app()->getLocale() }}">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <title>Aplikasi Perpustakaan</title>
-    <link rel="stylesheet" href="{{asset('bower_components/bootstrap/dist/css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link href="{{ asset('css/login.css') }}" rel="stylesheet">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <title>{{ $nama_perpus }} | Log in</title>    
+    @include('layouts._styles')
+    <style>
+        .logo {
+            width: 82px;            
+        }
+    </style>
 </head>
-<body style="background:#ECF0F5;">
-    <section>
-        <div class="signinpanel">
-            <div class="row">
-                <div class="col-md-6 col-md-offset-3">
-                    <div class="signin-info">
-                        <div class="thumbnail">
-                            <a href="http://localhost/sisfoatlet/public/home">
-                                {!! Html::image(asset('img/logo/'.$logo),null,['class' => 'img-responsive']) !!}
-                                <div class="caption text-center">
-                                    <h5>
-                                        <strong>
-                                            Selamat Datang di Aplikasi {{ $nama_perpus }}, Silahkan
-                                            Gunakan Username dan Password Anda Untuk Masuk ke Aplikasi!
-                                        </strong>
-                                    </h5>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-6 col-md-offset-3">
-                    <div class="wrapper">
-                        <div class="container">
-                            {!! Form::open(['route' => 'login','class' => 'form-horizontal']) !!}
-                            {{ csrf_field() }}
-                            <div class="form-group{{ $errors->has('username') ? ' has-error' : '' }}">                            
-                                {!! Form::text('username',null,['class' => 'field-input']) !!}
-                                {!! $errors->first('username','<span class="help-block"><strong>:message</strong></span>') !!}
-                            </div>
 
-                            <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                                {!! Form::password('password',['class' => 'field-input']) !!}
-                                {!! $errors->first('password', '<span class="help-block"><strong>:message</strong></span>') !!}
-                            </div>
-                            <button type="submit" id="login-button" class="btn btn-primary">
-                                Login
-                            </button>                           
-                            {!! Form::close() !!}
-                        </div>
-
-                        <ul class="bg-bubbles">
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-6 col-md-offset-3">
-                    <div class="signup-footer">
-                        <p>Copyright © 2018 <a href="#" target="_blank">Nur Hakim</a>. All rights reserved.</p>                        
-                    </div>
-                </div>
-            </div>
+<body class="hold-transition login-page">
+    <div class="login-box">
+        <div class="login-logo">
+            <a href="{{ url('/') }}">
+                {!! Html::image(asset('img/logo/'.$logo),null,['class' => 'logo']) !!}                
+            </a>
         </div>
-    </section>
+        <!-- /.login-logo -->
+        @if ($errors->any())
+            <div class="alert alert-danger pesan-error">            
+                Gagal! Username Atau Password Salah!
+            </div>
+        @endif
+
+        <div class="login-box-body">
+            <p class="login-box-msg">Silahkan Gunakan Username dan Password Anda Untuk Masuk ke Aplikasi!</p>
+
+            {{-- <form action="../../index2.html" method="post"> --}}
+                {!! Form::open(['route' => 'login']) !!}                
+                <div class="form-group has-feedback">                    
+                    {!! Form::text('username',null,['class' => 'form-control', 'placeholder' => 'Username']) !!}
+                    <span class="glyphicon glyphicon-user form-control-feedback"></span>
+                </div>
+                <div class="form-group has-feedback">
+                    {!! Form::password('password',['class' => 'form-control', 'placeholder' => 'Password']) !!}                    
+                    <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+                </div>
+                <div class="row">
+                    <div class="col-xs-4 col-xs-offset-8">
+                        <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
+                    </div>
+                    <!-- /.col -->
+                </div>
+            {{-- </form> --}}
+            {!! Form::close() !!}
+        </div>
+        <!-- /.login-box-body -->
+    </div>
+    <!-- /.login-box -->
+    <script>  
+        // (function() {
+        //     setTimeout(function () {
+        //         document.querySelector(".pesan-error").remove();
+        //     }, 5000); 
+        // })();
+    </script>
 </body>
 </html>
