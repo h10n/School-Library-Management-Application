c1d848f90570a0762b301f920b718e67f164f69d
h10n <nurhakim.nh10@gmail.com>
2021-10-19 17:09:19 +0800

    ekspor > Daftar

--- resources/views/pdf/laporan/ekspor.blade.php
+++ resources/views/pdf/laporan/ekspor.blade.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Ekspor {{ ucfirst($request['jenis']) }}</title>
+  <title>Daftar {{ ucfirst($request['jenis']) }}</title>
   <link href="{{ asset('css/kop.css') }}" rel="stylesheet" media="screen">
   <link href="{{ asset('css/laporan.css') }}" rel="stylesheet" media="screen">
 </head>
@@ -16,7 +16,7 @@
   </header>
   
   @if (!empty($data))
-  <h3>Ekspor {{ ucfirst($request['jenis']) }} </h3>
+  <h3>Daftar {{ ucfirst($request['jenis']) }} </h3>
   <h5>Periode : {{ $request['periode_awal'] }} s.d {{ $request['periode_akhir'] }}
   {{-- deprecated, min php 7.3--}}
   {{-- <h5>Periode : {{ $firstKey = array_key_first($visitor_bulanan) }} s.d {{ $firstKey = array_key_last($visitor_bulanan) }} --}}
