5ae52032f2012c490e132c332452cbbf573fc989
h10n <nurhakim.nh10@gmail.com>
2021-11-06 18:39:11 +0800

    +CarouselController refactor

--- app/Http/Controllers/CarouselController.php
+++ app/Http/Controllers/CarouselController.php
@@ -13,61 +13,54 @@ use Illuminate\Support\Facades\Storage;
 
 class CarouselController extends Controller
 {
-    use FlashNotificationTrait,UploadFileTrait;
+    use FlashNotificationTrait, UploadFileTrait;
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    /*  public function index()
-      {
-        $carousels = Carousel::orderby('id', 'desc')->paginate(10);
-        return view('carousels.index', compact('carousels')); //sementara view nya gk usah dipisah
-      }*/
-    //index with htmlBuilder
-
     public function index(Request $request, Builder $htmlBuilder)
     {
         if ($request->ajax()) {
-            $carousels = Carousel::select(['id','title','text','img'])->latest('updated_at')->get();
+            $carousels = Carousel::select(['id', 'title', 'text', 'img'])->latest('updated_at')->get();
             return Datatables::of($carousels)
-    ->addColumn('image', function ($carousels) {
-        if (!$carousels->img) {
-            return '';
-        }
-        return view('carousels.img', ['imgcarousel' => $carousels->img]);
-    })
-    ->addColumn('action', function ($carousel) {
-        return view('datatable._carousel-action', [
-        'model' => $carousel,
-        'form_url' => route('carousels.destroy', $carousel->id),
-        'edit_url' => route('carousels.edit', $carousel->id),
-        'title' => 'Slider',
-        'confirm_message' => 'Yakin ingin menghapus '.$carousel->title.' ?'
-      ]);
-    })
-    ->rawColumns(['image','action'])
-    ->addIndexColumn()
-    ->make(true);
+                ->addColumn('image', function ($carousels) {
+                    if (!$carousels->img) {
+                        return '';
+                    }
+                    return view('carousels.img', ['imgcarousel' => $carousels->img]);
+                })
+                ->addColumn('action', function ($carousel) {
+                    return view('datatable._carousel-action', [
+                        'model' => $carousel,
+                        'form_url' => route('carousels.destroy', $carousel->id),
+                        'edit_url' => route('carousels.edit', $carousel->id),
+                        'title' => 'Slider',
+                        'confirm_message' => 'Yakin ingin menghapus ' . $carousel->title . ' ?'
+                    ]);
+                })
+                ->rawColumns(['image', 'action'])
+                ->addIndexColumn()
+                ->make(true);
         }
 
         $html = $htmlBuilder
-  ->addColumn([
-    'defaultContent' => '',
-    'data'           => 'DT_Row_Index',
-    'name'           => 'DT_Row_Index',
-    'title'          => '',
-    'render'         => null,
-    'orderable'      => false,
-    'searchable'     => false,
-    'exportable'     => false,
-    'printable'      => true,
-    'footer'         => '',
-])
-  ->addColumn(['data' => 'title', 'name' => 'title', 'title' => 'Judul'])
-  ->addColumn(['data' => 'text', 'name' => 'text', 'title' => 'Isi'])
-  ->addColumn(['data' => 'image', 'name' => 'image', 'title' => 'Gambar','orderable' => false, 'searchable' => false])
-  ->addColumn(['data' => 'action','name' => 'action','title' => '','orderable' => false,'searchable' => false]);
+            ->addColumn([
+                'defaultContent' => '',
+                'data'           => 'DT_Row_Index',
+                'name'           => 'DT_Row_Index',
+                'title'          => '',
+                'render'         => null,
+                'orderable'      => false,
+                'searchable'     => false,
+                'exportable'     => false,
+                'printable'      => true,
+                'footer'         => '',
+            ])
+            ->addColumn(['data' => 'title', 'name' => 'title', 'title' => 'Judul'])
+            ->addColumn(['data' => 'text', 'name' => 'text', 'title' => 'Isi'])
+            ->addColumn(['data' => 'image', 'name' => 'image', 'title' => 'Gambar', 'orderable' => false, 'searchable' => false])
+            ->addColumn(['data' => 'action', 'name' => 'action', 'title' => '', 'orderable' => false, 'searchable' => false]);
         return view('carousels.index')->with(compact('html'));
     }
 
@@ -89,37 +82,12 @@ class CarouselController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function store(CarouselRequest $request)
-    {     
+    {
         $carousel = Carousel::create($request->all());
         $this->uploadFile($request, $carousel, 'img_file', 'img', 'slider');
-        // $carousel = new Carousel;
-        // $carousel->title = $request->input('title');
-        // $carousel->text = $request->input('text');
-        // if ($request->hasFile('img')) {
-        //     $img = $request->file('img');
-        //     $filename = 'slide' . '-' . time() . '.' . $img->getClientOriginalExtension();
-        //     $location = public_path('img/slider/');
-        //     $request->file('img')->move($location, $filename);
-
-        //     $carousel->img = $filename;
-        // }
-        // $carousel->save();
-
-
         $this->sendFlashNotification('menambah', $carousel->title);
         return redirect()->route('carousels.index');
     }
-    
-    // protected function uploadPhoto($request, $model, $tmpFileField, $fileField, $dir){
-    //     if ($request->$tmpFileField) {
-    //       $oldfilename = $model->$fileField;
-    //       $fileName = $this->upload($dir, $request->$tmpFileField, $oldfilename);
-    //       if ($fileName) {
-    //           $model->$fileField = $fileName;
-    //           $model->save();
-    //       }            
-    //   }
-    //   }
     /**
      * Display the specified resource.
      *
@@ -152,27 +120,11 @@ class CarouselController extends Controller
      */
     public function update(CarouselRequest $request, $id)
     {
-        $carousel = Carousel::find($id);                    
-        if ($carousel->update($request->all())) {            
+        $carousel = Carousel::find($id);
+        if ($carousel->update($request->all())) {
             $this->uploadFile($request, $carousel, 'img_file', 'img', 'slider');
             $this->sendFlashNotification('mengubah', $carousel->title);
         }
-
-        // if ($request->hasFile('img')) {
-        //     $img = $request->file('img');
-        //     $filename = 'slide' . '-' . time() . '.' . $img->getClientOriginalExtension();
-        //     $location = public_path('img/slider');
-        //     $request->file('img')->move($location, $filename);
-
-        //     $oldFilename = $carousel->img;
-        //     $carousel->img= $filename;
-        //     $carousel->save();
-
-        //     if (!empty($carousel->img)) {
-        //         Storage::delete($oldFilename);
-        //     }
-        // }
-
         return redirect()->route('carousels.index');
     }
 
@@ -186,7 +138,9 @@ class CarouselController extends Controller
     {
         $item = Carousel::findOrFail($id);
         Storage::delete($item->img);
-        if(!$item->delete()) return redirect()->back(); 
+        if (!$item->delete()) {
+            return redirect()->back();
+        }
         $this->deleteFile('slider', $item->img);
         $this->sendFlashNotification('menghapus', $item->title);
         return redirect()->route('carousels.index');
