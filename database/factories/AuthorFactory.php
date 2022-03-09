7288998645be2e81ff0be011c88341c87ff598b8
Nur Hakim <nurhakim.nh10@gmail.com>
2022-03-09 17:58:53 +0800

    +singkatan > up

--- database/factories/AuthorFactory.php
+++ database/factories/AuthorFactory.php
@@ -6,6 +6,6 @@ $factory->define(App\Author::class, function (Faker $faker) {
     $name = $faker->name;
     return [        
         'name' => $name,     
-        'singkatan' => substr($name, 0, 3)   
+        'singkatan' => strtoupper(substr($name, 0, 3))   
     ];
 });
