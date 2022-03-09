0cc508992e7688db78d9d4251aa863d95cd28d2e
Nur Hakim <nurhakim.nh10@gmail.com>
2022-03-10 01:08:27 +0800

    -pic

--- database/seeds/MembersSeeder.php
+++ database/seeds/MembersSeeder.php
@@ -13,9 +13,9 @@ class MembersSeeder extends Seeder
      */
     public function run()
     {
+        Member::create(['no_induk' => '123456',  'name' => 'Sampel Anggota', 'kelas' => '', 'jurusan' => '', 'jenis_anggota' => 'guru/staff', 'address' => '', 'email' => 'member@perpus.com', 'phone' => '', 'photo' => '']);
         $count = 100;
         factory(Member::class, $count)->create();          
-        Member::create(['no_induk' => '123456',  'name' => 'Sampel Anggota', 'kelas' => '', 'jurusan' => '', 'jenis_anggota' => 'guru/staff', 'address' => '', 'email' => 'member@perpus.com', 'phone' => '', 'photo' => '']);
       
         $members =  Member::all();
         foreach ($members as $key => $member) {
